802ee8b3bf5980e4078fe2b500d9af1eabbf10be
Kyle Erhabor <kyleerhabor@gmail.com>
2025-04-10 14:14:11 -0400

    Display map

--- script.js
+++ script.js
@@ -14,24 +14,34 @@ document.querySelectorAll('.btn').forEach(button => {
         ripple.style.left = e.clientX - e.target.offsetLeft + 'px';
         ripple.style.top = e.clientY - e.target.offsetTop + 'px';
         this.appendChild(ripple);
-        
+
         setTimeout(() => ripple.remove(), 500);
     });
 });
 
-// Search functionality
-document.querySelector('.search-btn').addEventListener('click', () => {
-    const query = document.querySelector('input').value;
-    // Implement search logic
-    console.log('Searching for:', query);
-});
+// // Search functionality
+// document.querySelector('.search-btn').addEventListener('click', () => {
+//     const query = document.querySelector('input').value;
+//     // Implement search logic
+//     console.log('Searching for:', query);
+// });
 
-// Smooth scroll for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+// // Smooth scroll for anchor links
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+
+// Map
+
+// Rosalie K. Stahl Center: 73 Tremont Street, Boston, Massachusetts
+const map = L.map("map").setView([42.357813, -71.061027], 18)
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+})
+.addTo(map);
